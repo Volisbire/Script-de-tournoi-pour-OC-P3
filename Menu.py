8e0636d4594bdbb526b0500e3d4197c6a0f0137c
Volisbire <Volisbire@protonmail.com>
2021-11-10 19:48:21 +0100

    2nd commit

--- Menu.py
+++ Menu.py
@@ -1,11 +1,29 @@
-from Players import *
-from Tournament import *
+from Players import fake_players
+from Tournament import Tournoi
+from Matchs import Matchs
+from Round import Round
 
 
 def bullet_start():
-    joueurs_en_lice = players_dictionnary()
-    premier_tri(joueurs_en_lice)
-    print("GO !")
+    print("Go ")
+    jo = fake_players()
+    jolito = jo.values()
+    tournoi = Tournoi("tournoi de ouf", "Paris", "21/07/81", jolito, "10:10", "bogoss")
+    first_matchs_list = tournoi.match_list_by_rank()
+    first_round = Round(first_matchs_list, "Round 1")
+    n = 0
+    result = Matchs(first_matchs_list[n].firstname, first_matchs_list[n+4].firstname)
+    winner = input("Indiquez le nom du joueur ayant gagné : ")
+    if winner == first_matchs_list[n].firstname:
+        result.player1_win()
+    elif winner == first_matchs_list[n+4].firstname:
+        result.player2_win()
+    else:
+        result.draw()
+    n += 1
+    print(first_matchs_list[n].firstname)
+    print(first_round.name, first_round.creation)
+    print(result.tuplematch)
 
 
 def blitz_start():
@@ -35,4 +53,3 @@ def menu():
 
 
 menu()
-gagnant = input("indiquer le numéro du joueur l'ayant emporte (ex: 1 pour le joueur 1): ")
--- Round.py
+++ Round.py
@@ -1,11 +1,13 @@
 from typing import List
 from datetime import datetime
+from Matchs import Matchs
 
 
 class Round:
-    def __init__(self, match: List[Match], name):
+    def __init__(self, match: List[Matchs], name):
         self.end = None
         self.name = name
+        self.match = match
         self.creation = datetime.now()
 
     def end_round(self):
